ec2cd86b34bd16827c9c9e1abadeca789447ad01
Michail Gilev <mishagilevfgh@e1.ru>
2023-11-26 13:01:58 +0300

    TEMP

--- parser/Parser.py
+++ parser/Parser.py
@@ -1,16 +1,22 @@
+from urllib.parse import urlparse
 import re
+
 
 class Parser:
     def __init__(self):
         self.dict_parser = {}
-    def parse_path(self, path) -> dict:
+    def parse_path(self, path: str) -> dict:
         self.dict_parser['path_from_root'] = re.findall('/(.*?)/', path)
         self.dict_parser['params'] = set(re.findall("{(.*?)}", path))
         return self.dict_parser
-    
+    def parser_url(self, url_path: str):
+        result = urlparse(url_path)
+        return result
 
 temp = Parser()
 result = temp.parse_path('/all/{temp}/{temp2}')
-
 print(result)
+
+res2 = temp.parser_url('http://www.cwi.nl/%7Eguido/Python.html')
+print(res2)
 
