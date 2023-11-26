f27bea8d75e3eb19557b23ffeb8a5999eedb0b04
EternalTLD <shvejkin01@mail.ru>
2023-11-26 15:06:38 +0300

    Added select, from and where statements

--- orm/query.py
+++ orm/query.py
@@ -2,6 +2,24 @@ from typing import Any
 from abc import abstractmethod
 
 
+AND = "and"
+OR = "or"
+
+
+class Q:
+
+    def __init__(self, op_type: str = AND, **kwargs) -> None:
+        self.separator = op_type
+        self._params = kwargs
+
+    def __str__(self) -> str:
+        kv_pairs = [f"{k} = {v}" for k, v in self._params.items()]
+        return f" {self.separator}".join(kv_pairs)
+    
+    def __bool__(self) -> bool:
+        return bool(self._params)
+
+
 class BaseStatement:
     name = None
 
@@ -19,3 +37,54 @@ class BaseStatement:
     @abstractmethod
     def __bool__(self) -> bool:
         pass
+
+
+class Select(BaseStatement):
+    name = "SELECT"
+
+    def __init__(self) -> None:
+        self._params = []
+
+    def add(self, *args, **kwargs):
+        self._params.extend(*args)
+
+    def line(self) -> str:
+        separator = ","
+        return separator.join(self._params)
+    
+    def __bool__(self) -> bool:
+        return bool(self._params)
+
+
+class From(BaseStatement):
+    name = "FROM"
+
+    def __init__(self) -> None:
+        self._params = []
+
+    def add(self, *args, **kwargs):
+        self._params.extend(*args)
+
+    def line(self) -> str:
+        separator = ","
+        return separator.join(self._params)
+    
+    def __bool__(self) -> bool:
+        return bool(self._params)
+    
+
+class Where(BaseStatement):
+    name = "WHERE"
+
+    def __init__(self, op_type: str = AND, **kwargs) -> None:
+        self._q = Q(op_type, **kwargs)
+
+    def add(self, op_type: str = AND, **kwargs):
+        self._q = Q(op_type, **kwargs)
+        return self._q
+
+    def line(self) -> str:
+        return str(self._q)
+    
+    def __bool__(self) -> bool:
+        return bool(self._q)
