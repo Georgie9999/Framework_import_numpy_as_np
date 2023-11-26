697d04db39a105e444a38a2b9c8373cec89ff45f
RST <rstkachev@gmail.com>
2023-11-26 15:48:01 +0300

    updated parser

--- http_parser.py
+++ http_parser.py
@@ -3,22 +3,28 @@ def parse_http(response):
 
     response = response.decode()
 
-    response_list = response.split('\r\n')
+    response_structure, response_headers = response.split('\r\n', maxsplit=1)
 
-    response_parsed["method"], response_parsed["url"], response_parsed["version"] = response_list[0].split(sep="/")
+    response_parsed["method"], temp_response = response_structure.split(sep=" /", maxsplit=1)
 
-    _, response_parsed["host"] = response_list[1].split(": ", maxsplit=1)
+    response_parsed["url"], temp_response = temp_response.split(' ', maxsplit=1)
+
+    if " " in temp_response:
+        response_parsed["version"], response_parsed["code"] = temp_response.split(" ", maxsplit=1)
+    else:
+        response_parsed["version"] = temp_response
 
     headers = {}
 
-    for i in range(2, len(response_list) - 2):
-        header, body = response_list[i].split(": ", maxsplit=1)
-        headers[header] = body
+    for line in response_headers.split('\r\n'):
+        if line:
+            header, body = line.split(": ", maxsplit=1)
+            headers[header] = body
 
     response_parsed["headers"] = headers
 
     response_parsed["body"] = ""
-    if response_list[-2]:
-        _, response_parsed["body"] = response_list[-2].split(": ")
+    if response_headers.split('\r\n')[-2]:
+        _, response_parsed["body"] = response_headers[-2].split(": ")
 
     return response_parsed
