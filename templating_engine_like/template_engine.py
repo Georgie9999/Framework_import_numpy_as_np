a61f1a6d017e7c8fdfa96a4a9f56dec058f64646
marcheanin <marcheanin1576@gmail.com>
2023-11-26 16:42:02 +0300

    add if dealing

--- templating_engine_like/template_engine.py
+++ templating_engine_like/template_engine.py
@@ -23,12 +23,59 @@ class TemplateEngine:
     def replace_substitutions(self):
         for key in self.parameters:
             sub_str = "{{" + f"{key}" + "}}"
-            self.template = re.sub(sub_str, self.parameters[key], self.template, count=0)
+            self.template = re.sub(sub_str, str(self.parameters[key]), self.template, count=0)
 
         return self.template
 
+    def extract_if(self, pos: int):
+        i = pos
+
+        while not (self.template[i] == '%' and self.template[i + 1] == '}'):
+            i += 1
+
+        pos_end_if = i
+        if_statement = self.template[pos + 2:pos_end_if]
+
+        pos_end_if_true_data = self.template.find("{% else %}", i)
+        if_true_data = self.template[pos_end_if+2:pos_end_if_true_data]
+
+        i = pos_end_if_true_data + 9
+        pos_start_else_data = i
+
+        pos_end_else_data = self.template.find("{% endif %}")
+        else_data = self.template[pos_start_else_data+1:pos_end_else_data]
+
+        i = pos_end_else_data
+        endif_pos = i + 11
+
+        return if_statement, if_true_data, else_data, endif_pos
+
+    def eval_if_statement(self, if_statement):
+        if_statement = if_statement[2:]
+        res = eval(if_statement, self.parameters)
+        return res
+
     def if_dealing(self):
-        pass
+        f = True
+        while(f):
+            f = False
+            for i in range(len(self.template) - 3):
+                if self.template[i] == '{' and self.template[i + 1] == '%' and self.template[i + 3] == 'i':
+                    f = True
+                    if_statement, if_true_data, else_data, endif_pos = self.extract_if(i + 1)
+                    print('state: ', if_statement, if_true_data, else_data, endif_pos, sep='\n')
+                    if self.eval_if_statement(if_statement):
+                        self.template = self.template.replace(else_data, "", i)
+                        endif_pos -= len(else_data)
+                    else:
+                        self.template = self.template.replace(if_true_data, "", i)
+                        endif_pos -= len(if_true_data)
+                    self.template = self.template.replace("{% " + if_statement + "%}", "", i)
+                    i = endif_pos
+                    break
+
+        self.template = self.template.replace("{% endif %}", "")
+        self.template = self.template.replace("{% else %}", "")
 
     def for_dealing(self):
         pass
--- templating_engine_like/test_temp_engine.py
+++ templating_engine_like/test_temp_engine.py
@@ -5,8 +5,13 @@ if __name__ == '__main__':
 
     temp.set_template_from_html('../templates/first_html.html')
     temp.set_parameters(name="mem", lastname="cringe")
-    temp.replace_substitutions()
+    temp.set_parameters(name="mem", lastname="cringe", score=90)
 
     print(temp.template)
     print(temp.parameters)
+    temp.if_dealing()
 
+    temp.eval_if_statement("if len(name) < 5")
+
+    temp.replace_substitutions()
+    print(temp.template)
