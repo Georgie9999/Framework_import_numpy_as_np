2802d247d013edbab95bc3c04639a61536b88a53
Maksim Kononykhin <kononelder@gmail.com>
2023-11-26 13:41:54 +0300

    stage sommit

--- bestapi.py
+++ bestapi.py
@@ -1,3 +1,9 @@
+from asyncio import Queue
+import asyncio
+
+import requests
+
+
 class BestApi:
     def __init__(self):
         self.get_dict = {}
@@ -5,26 +11,54 @@ class BestApi:
         self.put_dict = {}
         self.delete_dict = {}
 
-    def get(self, path):
+        self.loop = asyncio.get_event_loop()
+
+        self.server = self.loop.run_until_complete(
+            asyncio.start_server(
+                self.__route, port=1337
+            )
+        )
+    
+    def run(self):
+        self.loop.run_forever()
+
+    def get(self, path: str):
         def inner_get(func):
             self.get_dict[path] = func
 
         return inner_get
     
-    def put(self, path):
+    def put(self, path: str):
         def inner_put(func):
             self.put_dict[path] = func
 
         return inner_put
     
-    def post(self, path):
+    def post(self, path: str):
         def inner_post(func):
             self.post_dict[path] = func
 
         return inner_post
     
-    def delete(self, path):
+    def delete(self, path: str):
         def inner_delete(func):
             self.delete_dict[path] = func
 
         return inner_delete
+
+    async def __route(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
+        while True:
+            data = await asyncio.wait_for(
+                reader.readline(), timeout=10
+            )
+            print(data)
+            # if data is not None:
+            #     raw_response = await self.get_dict['/hello']()
+
+app = BestApi()
+
+@app.get('/hello')
+async def rand_func():
+    return 'hello'
+
+app.run()
